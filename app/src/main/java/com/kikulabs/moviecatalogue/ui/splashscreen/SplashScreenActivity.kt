541c02e41b07e6d57cdf29d26abd457f42e18953
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-04 23:21:17 +0700

    Created splash screen

---
+++ app/src/main/java/com/kikulabs/moviecatalogue/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.kikulabs.moviecatalogue.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.kikulabs.moviecatalogue.R
+import com.kikulabs.moviecatalogue.ui.home.HomeActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
