09a09cdfb723ec1a6eaa94732eb74ed7add72f84
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-04 23:30:43 +0700

    Done detail

--- app/src/main/java/com/kikulabs/moviecatalogue/ui/detail/DetailMovieActivity.kt
+++ app/src/main/java/com/kikulabs/moviecatalogue/ui/detail/DetailMovieActivity.kt
@@ -2,15 +2,22 @@ package com.kikulabs.moviecatalogue.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.kikulabs.moviecatalogue.R
+import com.kikulabs.moviecatalogue.data.DataEntity
 import com.kikulabs.moviecatalogue.databinding.ActivityDetailMovieBinding
 import com.kikulabs.moviecatalogue.databinding.ContentDetailMovieBinding
-import com.kikulabs.moviecatalogue.utils.DataDummy
 
 class DetailMovieActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_MOVIE = "extra_movie"
+        const val EXTRA_TYPE = "extra_type"
     }
+
+    private lateinit var content: DataEntity
 
     private lateinit var detailContentBinding: ContentDetailMovieBinding
 
@@ -25,12 +32,42 @@ class DetailMovieActivity : AppCompatActivity() {
         setSupportActionBar(activityDetailMovieBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        )[DetailMovieViewModel::class.java]
+
         val extras = intent.extras
         if (extras != null) {
-            val movieId = extras.getString(EXTRA_MOVIE)
-            if (movieId != null) {
+            val id = extras.getString(EXTRA_MOVIE)
+            val type = extras.getString(EXTRA_TYPE)
 
+            if (type.equals("MOVIE", ignoreCase = true)) {
+                if (id != null) {
+                    viewModel.setSelectedMovie(id)
                 }
+                content = viewModel.getMovie()
+            } else if (type.equals("TV_SHOW", ignoreCase = true)) {
+                if (id != null) {
+                    viewModel.setSelectedTvShow(id)
+                }
+                content = viewModel.getTvShow()
+                supportActionBar?.title = "Detail Tv Show"
             }
+
+            detailContentBinding.tvTitle.text = content.title
+            detailContentBinding.tvReleaseDate.text = content.releaseDate
+            detailContentBinding.tvRating.text = content.rating
+            detailContentBinding.tvLanguage.text = content.language
+            detailContentBinding.tvOverviewValue.text = content.overview
+
+            Glide.with(this)
+                .load(content.poster).apply(
+                    RequestOptions.placeholderOf(R.drawable.ic_loading)
+                        .error(R.drawable.ic_error)
+                )
+                .into(detailContentBinding.ivMovie)
+
+        }
     }
 }
